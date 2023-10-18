1232a4bb2860e8ea467c554d3bae990eab5cc6b0
tuNguyenGithub <tu.nguyen@egany.com>
2023-10-18 11:24:34 +0700

    Redux overall

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const redux = require('redux')
-const createStore = redux.createStore
+const redux = require("redux");
+const createStore = redux.createStore;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+
+const initialState = {
+  numOfCakes: 10,
+};
 
 // Action
 function orderCake() {
@@ -10,10 +15,12 @@ function orderCake() {
     quantity: 1,
   };
 }
-
-const initialState = {
-  numOfCakes: 10,
-};
+function restockCake(payload = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload,
+  };
+}
 
 // Reducer
 const reducer = (state = initialState, action) => {
@@ -22,20 +29,26 @@ const reducer = (state = initialState, action) => {
       return {
         numOfCakes: state.numOfCakes - 1,
       };
-
+    case CAKE_RESTOCKED:
+      return {
+        numOfCakes: state.numOfCakes + action.payload,
+      };
     default:
       return state;
   }
 };
 
 // Store
-const store = createStore(reducer)
-console.log('initial state: ', store.getState());
+const store = createStore(reducer);
+console.log("initial state: ", store.getState());
 
-const unsubscribe = store.subscribe(() => console.log('Update state: ', store.getState()))
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state: ", store.getState())
+);
 
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(3))
 
-unsubscribe()
+unsubscribe();
